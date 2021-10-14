2622626c9319a9e85db0b80ae1174dc65a4c8a86
ㅤelfaalbirri <elfaalbirri19@gmail.com>
2021-10-14 18:53:20 +0700

    First Commit

---
+++ resources/views/main.blade.php
@@ -0,0 +1,143 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+    <title>Web Profile | Elfal Birri Firamadhani</title>
+  </head>
+  <body id="home">
+
+  <!-- Navbar -->
+  <nav class="navbar navbar-expand-lg navbar-dark p-3 fixed-top" style="background-color: #8D7D6E;">
+  <div class="container">
+    <a class="navbar-brand" href="#home">Elfal Birri Firamadhani</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav ms-auto">
+        <li class="nav-item">
+          <a class="nav-link" href="#about">About</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#skills">Skills</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#experience">Experience</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#contact">Contact</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+  <!-- End Navbar -->
+
+  <!-- Header -->
+
+        <img src="img/header.jpg" class="img-fluid" alt="Header">  
+
+  <!-- End Header -->
+
+  <!-- About -->
+
+    
+    <div class="container p-5" id="about">
+        <div class="row">
+            <div class="col-4">
+                <img src="img/elfal.jpeg" alt="Elfal Birri Firamadhani" width="300">
+            </div>
+            <div class="col-6">
+                <h2>About Me</h2>
+</br>
+                <p>My name is Elfal Birri Firamadhani,  19 years old. I was born and raised in Pasuruan, East Java. I am an undergraduated Informatics Engineering student who actively join organizations and volunteerings
+                program. 
+</br>
+</br>
+                Since i was in Elementary School, i have a dream to become an author some day. And now i’m starting to write my own story to make that dream comes true.
+</br>
+</br>
+                I’m interested in design and keep learning day by day to improve my skill. Beside that, i’m also interested to learning about UI UX Design. 
+                </p>
+            </div>
+        </div>
+    </div>
+
+  <!-- End About -->
+
+  <!-- Skills -->
+
+    <div class="container" id="skills">
+        <div class="row text-center mt-5">
+            <div class="col">
+                <h2>My Skills</h2>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col text-center">
+                <img src="img/time_manag.jpg" alt="Time Management" width="150">
+                <p class="fs-5">Time Management</p>
+            </div>
+            <div class="col text-center">
+                <img src="img/teamwork.jpg" alt="Teamwork" width="150">
+                <p class="fs-5">Teamwork</p>
+            </div>
+            <div class="col text-center">
+                <img src="img/planning.jpg" alt="Planning" width="150">
+                <p class="fs-5">Planning</p>
+            </div>
+            <div class="col text-center">
+                <img src="img/adaptation.jpg" alt="Adaptation" width="150">
+                <p class="fs-5">Adaptation</p>
+            </div>
+        </div>
+    </div>
+
+  <!-- End Skills -->
+
+  <!-- Experience -->
+
+    <div class="container p-5" id="experience">
+            <h2>Experiences</h2>
+</br>
+            <p class="fs-5">Member of Himpunan Mahasiswa Jurusan Teknik Informatika (2020-now)</p>
+            <p>Joining the Department of Research and Technology is a Department that conducts research and development of functionary work systems and acts as facilitators and mediators in the professional and academic fields to realize superior, active, accomplished himti.</p>
+</br>
+            <p class="fs-5">Secretary of Mengelola Emosi Jawa Timur (May-July 2021)</p>
+            <p>Responsible in the field of administration, replacing the chairman if not present at weekly meetings, recapitulating the list of attendance of members in the organization's activities every month, recording the results of weekly meetings.</p>
+</br>
+            <p class="fs-5">Member of Biro Riset dan Keanggotaan Literasi Muda Berkarya (Sept 2021-now)</p>
+            <p>Responsible for collecting, managing, and analyzing data needed for organizational purposes, creating a database of each member of the organization, carrying out evaluations, compiling the measurement tools needed for each organizational activity.</p>
+    </div>
+
+  <!-- End Experience -->
+
+  <!-- Contact -->
+
+    <div class="contact text-light" style="background-color: #8D7D6E;" id="contact">
+      <div class="container p-5">
+        <h2>Contact Me</h2>
+        <p>Perumahan Gempol Citra Asri Blok F/10 Gempol, Pasuruan, Jawa Timur 67155</p>
+</br>
+</br>
+</br>
+
+        <p>08993321147── elfaalbirri19@gmail.com</p>
+      </div>
+    </div>
+
+  <!-- End Contact -->  
+
+    
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+
+   
+  </body>
+</html>
